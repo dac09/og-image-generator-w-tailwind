81c617c88bd6d90b41ea974d5b0170441ee21853
Daniel Choudhury <dannychoudhury@gmail.com>
2024-04-16 22:24:57 +0700

    Register ogImageMiddleware

--- web/src/entry.server.tsx
+++ web/src/entry.server.tsx
@@ -1,35 +1,21 @@
-import { matchPath } from '@redwoodjs/router'
-import { MiddlewareResponse } from '@redwoodjs/vite/middleware'
+import OgImageMiddleware from '@redwoodjs/ogimage-gen/middleware'
 
 import App from './App'
 import { Document } from './Document'
-import OgImageMiddleware from './lib/OgImageMiddleware'
-
-import './ogStuff'
 
 interface Props {
   css: string[]
   meta?: any[]
 }
 
-const getRoutesList = async () => {
-  if (process.env.NODE_ENV === 'development') {
-    const { getProjectRoutes } = await import('@redwoodjs/internal/dist/routes')
-    return getProjectRoutes()
-  } else {
-    // @TODO what do we do in production?
-  }
-}
+// console.log('Register middleware called', await getRoutesList())
+export const registerMiddleware = () => {
+  const ogMw = new OgImageMiddleware({
+    App,
+    Document,
+  })
 
-export const registerMiddleware = async () => {
-  console.log('Register middleware called', await getRoutesList())
-
-  return [
-    // [
-    //   ogMw,
-    //   '*',
-    // ],
-  ]
+  return [ogMw]
 }
 
 export const ServerEntry: React.FC<Props> = ({ css, meta }) => {
