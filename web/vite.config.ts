fbe409b412fc525f1ec4f876d80f524c1ff52e0d
Daniel Choudhury <dannychoudhury@gmail.com>
2024-04-16 22:24:41 +0700

    Add vite plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,20 @@
-import dns from 'dns';
-import type { UserConfig } from 'vite';
-import { defineConfig } from 'vite';
-import redwood from '@redwoodjs/vite';
+import dns from 'dns'
+
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+
+import vitePluginOgImageGen from '@redwoodjs/ogimage-gen/plugin'
+import redwood from '@redwoodjs/vite'
 
 // So that Vite will load on localhost instead of `127.0.0.1`.
 // See: https://vitejs.dev/config/server-options.html#server-host.
-dns.setDefaultResultOrder('verbatim');
+dns.setDefaultResultOrder('verbatim')
+
 const viteConfig: UserConfig = {
-  plugins: [redwood()],
+  plugins: [redwood(), vitePluginOgImageGen()],
   optimizeDeps: {
-    force: true
-  }
-};
-export default defineConfig(viteConfig);
+    force: true,
+  },
+}
+
+export default defineConfig(viteConfig)
